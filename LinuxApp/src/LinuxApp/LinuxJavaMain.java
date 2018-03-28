e680a7644843c585ed5384c6be683569bae3cf8e
bit <bit@DESKTOP-010721N>
2018-03-28 15:21:50 +0900

    1 2

---
+++ LinuxApp/src/LinuxApp/LinuxJavaMain.java
@@ -0,0 +1,10 @@
+package LinuxApp;
+
+public class LinuxJavaMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Linux!!github");
+	}
+
+}
